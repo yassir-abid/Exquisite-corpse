5f20b71e4ab1c978df0e996af135e5b69f34c001
yassir-abid <abid.yassir@gmail.com>
2022-09-02 10:37:47 +0200

    test controller method getCadex with a mock of cadex module

--- tests/controller.test.js
+++ tests/controller.test.js
@@ -1,5 +1,20 @@
 /* eslint-disable no-undef */
+const { cadex } = require('../app/services/cadex');
 const controller = require('../app/controllers/controller');
+
+jest.mock('../app/services/cadex', () => {
+    const originalModule = jest.requireActual('../app/services/cadex');
+
+    return {
+        ...originalModule,
+        cadex: {
+            ...originalModule.cadex,
+            generate: jest.fn(() => ({
+                glue: () => 'test',
+            })),
+        },
+    };
+});
 
 const mockResponse = {
     json: jest.fn((data) => data),
@@ -12,10 +27,15 @@ describe('Get cadex', () => {
         result = controller.getCadex('', mockResponse);
     });
 
+    it('should call cadex.generate', () => {
+        expect(cadex.generate).toHaveBeenCalled();
+        expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    });
+
     it('should call response.json', () => {
         expect(mockResponse.json).toHaveBeenCalled();
         expect(mockResponse.json).toHaveBeenCalledTimes(1);
-        expect(mockResponse.json).toHaveBeenCalledWith(result);
+        expect(mockResponse.json).toHaveBeenCalledWith('test');
     });
 
     it('should send a non empty string', () => {
