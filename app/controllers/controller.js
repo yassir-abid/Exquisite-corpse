075a8cb65f6f2eefb582fcfc5e08dc727eb6c73e
yassir-abid <abid.yassir@gmail.com>
2022-09-02 10:32:42 +0200

    route to get random sentence

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,3 +1,10 @@
-const controller = {};
+const { cadex } = require('../services/cadex');
+
+const controller = {
+    getCadex(request, response) {
+        const cadexObject = cadex.generate();
+        response.json(cadexObject.glue());
+    },
+};
 
 module.exports = controller;
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const router = require('./app/router');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
