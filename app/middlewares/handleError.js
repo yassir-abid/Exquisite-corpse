16467f65c5eeaef2d2364340c55c01da006d0bb0
yassir-abid <abid.yassir@gmail.com>
2022-09-05 13:47:51 +0200

    disable error logging

--- app/middlewares/handleError.js
+++ app/middlewares/handleError.js
@@ -1,13 +1,8 @@
 /* eslint-disable no-unused-vars */
 const path = require('path');
-const logger = require('../helpers/logger');
 
 const handleError = (error, _, response, next) => {
-    if (!error.status || error.status !== 404) {
-        logger.error(error);
-    } else {
-        console.log(error);
-    }
+    console.log(error);
 
     if (error.status === 404) {
         return response.status(404).sendFile(path.join(__dirname, '../.././public/page404.html'));
